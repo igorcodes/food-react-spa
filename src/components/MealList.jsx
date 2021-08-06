92142301ae17db3937fe80fcf44c460cbed8075b
IgorZakutajlo <87570302+igorcodes@users.noreply.github.com>
2021-08-06 12:04:44 +0300

    Update MealList.jsx

--- src/components/MealList.jsx
+++ src/components/MealList.jsx
@@ -2,9 +2,9 @@ import {Meal} from './Meal'
 
 function MealList({meals}) { //принимает список блюд
 
-    return <div className='list'> {/* // обходит цикло через мап этот список */}
+    return <div className='list'>  {/* // обходит циклом через мап этот список */}
         {meals.map((meal) => (
-            <Meal key={meal.idMeal} {...meal} />  //постман показывает ключ обьекта meal это idMeal
+            <Meal key={meal.idMeal} {...meal} /> 
         ))}
     </div>
 }
