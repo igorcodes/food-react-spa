29c223ce7b75f0a0409b4d18d449bbc3abf4111f
IgorZakutajlo <87570302+igorcodes@users.noreply.github.com>
2021-08-06 12:17:49 +0300

    Update App.js

--- src/App.js
+++ src/App.js
@@ -8,7 +8,7 @@ import {About} from './pages/About';
 import {Contact} from './pages/Contact';
 import {Category} from './pages/Category';
 import {NotFound} from './pages/NotFound';
-import {GordonRamsayRecipe} from './pages/GordonRamsayRecipe'; // загружаем страницу
+import {GordonRamsayRecipe} from './pages/GordonRamsayRecipe';
 
 function App() {
   return (
@@ -16,23 +16,21 @@ function App() {
       <Router basename='/food-react-spa'>
       <Header />
       <main className='container content'>
-{/* // слассы container и content тут будут разные странички */}
+
 {/* та часть которую мы хотим обернуть в многостраничность она должна быть обернута в роутер */}
         <Switch> {/* Switch позволяет остановиться на одной странице которой подходит наш запрос и дальше просто не проверять */}
         {/* Switch должен только роуты содержать внутри себя */}
 
-{/* path='/' - это путь по которому пользователи будут искать страницу и мы говорим что компонент который принадлежит этому пути называется component={} */} 
-{/* вот мой отдельный компонент который отвечает за дом страницу */}
+{/* path='/' - это путь по которому пользователи будут искать страницу, компонент который принадлежит этому пути component={} */} 
         <Route exact path='/' > {/* exact - точный путь */}
             <Home />
         </Route>
         <Route path='/about' component={About} />
         <Route path='/contacts' component={Contact} />
         <Route path='/category/:name' component={Category} />
-        <Route path='/meal/:id' component={GordonRamsayRecipe} /> {/* //создаем ей ссылку */}
+        <Route path='/meal/:id' component={GordonRamsayRecipe} />
 
-        {/* мы просто попадем в самый последний вариант и отрисуем нот фаунд  */}
-        <Route component={NotFound} /> {/* он отработает в том случае если ни один из верхних роутов не сработает */}
+        <Route component={NotFound} /> {/* отработает в том случае если ни один из верхних роутов не сработает, просто попадем в самый последний вариант и отрисуем нот фаунд */}
 
         </Switch>
       </main>
